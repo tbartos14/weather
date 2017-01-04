222e2f5b95ba24f4ade05cbda45a99722fa73fc7
tbartos14 <tbartos14@gmail.com>
2017-01-03 20:44:05 -0500

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,61 @@
+#Datetime,RecNbr,WS_mph_Avg,PAR_Den_Avg,WS_mph_S_WVT,WindDir_SD1_WVT,AirTF_Avg,Rain_in_Tot,RH,WindDir_D1_WVT
+
+import matplotlib.pyplot as plt
+import urllib.request
+import time
+from plotim import *
+
+print("Weather info extracted from:")
+url = "http://www.weather.unh.edu/data/" + time.strftime("%Y") + "/" + str(int(time.strftime("%j"))) + ".txt"
+print(url)
+
+weather = urllib.request.urlopen(url)
+data = weather.readlines()
+
+##def process(line):
+##    line = line.split(b" ", 1)
+##    line.pop(0)
+##    line = line[0].split(b",")
+##    line.pop(0)
+##    line.pop(0)
+##    return line
+
+def process(line):
+    result = line.split(b",")
+    newresult = []
+    for item in result[2:]:
+        item = float(item)
+        newresult.append(item)
+    return newresult[2:]
+
+linesset = []
+
+for line in data:
+    linesset.append(line[:-2])
+
+linesset.pop(0)
+
+newlines = []
+
+for line in linesset:
+    newlines.append(process(line))
+
+xpoints = []
+ypoints = []
+time = 0
+
+for line in newlines:
+    xpoints.append(time)
+    time = time + (1/60)
+    ypoints.append(line[-4])
+
+
+plt.plot(xpoints, ypoints)
+plt.ylabel("Temperature (F)")
+plt.xlabel("Time (hours)")
+plt.axis([0,24,None,None])
+plt.show()
+
+plot1 = linearplot()
+plot1.set_data(xpoints,ypoints)
+plot1.plot_data()
